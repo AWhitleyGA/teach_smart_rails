3fc5443cb29c35e48f6f8597bfb28ec706cff293
Andrew Whitley <awhitley1233@gmail.com>
2016-11-24 18:47:25 -0600

    defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :courses do
+    resources :assignments
+    resources :lessons
+  end
+
+  resources :memberships do
+    resources :grades
+    resources :attendances
+  end
+
 end
